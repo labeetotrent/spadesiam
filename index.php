371a39be7644e5463a74e39c024a9f4e4bea8dd0
Trent Brookes <trentbrookes@gmail.com>
2015-12-22 10:08:39 +1000

    color fixes

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="index.html">Home</a>
+                        <a href="index.php">Home</a>
                     </li>
                     <li>
                         <a href="#about">About Us</a>
